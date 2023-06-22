547c18034e69693fbb0e9d1d29bbd4982903d300
EmileBlngr <emile.boulanger2@gmail.com>
2023-06-22 19:53:37 +0200

    Clean App, création navigation et ajout Page d'accueil

---
+++ app/src/screens/HomeLocations.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomeLocations() {
+    return(
+        <div>
+            <h1>Accueil des locations !</h1>
+            <p>
+                Plateforme de location de logements :
+                Un site web permettant aux utilisateurs de rechercher, comparer et réserver des logements pour leurs séjours.
+                Affichage des annonces de location avec des descriptions détaillées, des photos et des commentaires d'utilisateurs.
+                Fonctionnalités de recherche avancée, de filtres et de paiement sécurisé en ligne.
+            </p> 
+        </div>
+
+    );
+}
+export default HomeLocations;
