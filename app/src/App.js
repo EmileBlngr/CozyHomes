a8e6a3f1d5c1b85ef8bbd2ddf4b8d9824d3e69ca
EmileBlngr <emile.boulanger2@gmail.com>
2023-06-24 18:18:55 +0200

    Correction problèmes _screen.tsx, écran global mis en place

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import HomeLocations from './screens/HomeLocations.tsx';
 import Screen from './screens/screen.tsx'
 import React from 'react';
+import './App.css';
 function App() {
   return (
-    <div>
+    <div className='AppContainer'>
       <Routes>
         <Route path="/" element={<Screen><HomeLocations/></Screen>} />
         <Route path="/Home" element={<Screen><HomeLocations/></Screen>} />
