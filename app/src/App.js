9e3998655abe4c618237e0521f8c25e73d638705
EmileBlngr <emile.boulanger2@gmail.com>
2023-06-25 18:18:11 +0200

    Définition nom d'onglets dans App.js et créations composants signin et signup

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,39 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation} from 'react-router-dom';
+
 import HomeLocations from './screens/HomeLocations.tsx';
+import SignInPage from './screens/connexion/SignInPage.tsx';
+import SignUpPage from './screens/connexion/SignUpPage.tsx';
 import Screen from './screens/screen.tsx'
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const routeName = getRouteName(location.pathname);
+    document.title = routeName;
+  }, [location]);
+
+  // Fonction pour obtenir le nom de la route en fonction du chemin
+  const getRouteName = (path) => {
+    switch (path) {
+      case '/':
+        return 'CozyHomes : Trouvez un logement';
+      case '/home':
+        return "CozyHomes : Trouvez un logement";
+      default:
+        return 'CozyHomes';
+    }
+  };
   return (
     <div className='AppContainer'>
       <Routes>
         <Route path="/" element={<Screen><HomeLocations/></Screen>} />
-        <Route path="/Home" element={<Screen><HomeLocations/></Screen>} />
+        <Route path="/home" element={<Screen><HomeLocations/></Screen>} />
+        <Route path="/sign-in" element={<Screen><SignInPage/></Screen>} />
+        <Route path="/sign-up" element={<Screen><SignUpPage/></Screen>} />
       </Routes>
     </div>
   );
